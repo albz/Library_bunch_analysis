990e067da841541249ae138a835c825228f5c545
stefanoromeo <stefano.romeo.173.1@gmail.com>
2018-03-20 15:14:27 +0100

    Iris particle cut added

--- particle_selector.py
+++ particle_selector.py
@@ -20,6 +20,9 @@ def particle_selector(strategy,value_min,value_max,x,y,z,px,py,pz,w):
     if strategy == 'gamma':
         x,y,z,px,py,pz,w=particle_selector_gamma(value_min,value_max,x,y,z,px,py,pz,w)
 
+    if strategy == 'iris':
+        x,y,z,px,py,pz,w=particle_selector_iris(value_min,value_max,x,y,z,px,py,pz,w)
+
     return x,y,z,px,py,pz,w
 
 ### --- END select strategy --- ###
@@ -37,6 +40,14 @@ def particle_selector_slice(Zmin,Zmax,X,Y,Z,Px,Py,Pz,W):
 def particle_selector_gamma(gamma_min,gamma_max,x,y,z,px,py,pz,w):
     gamma = np.sqrt(1.+px**2+py**2+pz**2)
     selected = np.asarray(gamma>=gamma_min) & np.asarray(gamma<=gamma_max)
+    x, y, z  = x[selected], y[selected], z[selected]
+    px,py,pz =px[selected],py[selected],pz[selected]
+    w = w[selected]
+    return x,y,z,px,py,pz,w
+
+def particle_selector_iris(radius_min,radius_max,x,y,z,px,py,pz,w):
+    radius_min=np.maximum(radius_min,0.)
+    selected = np.asarray(x**2+y**2>=radius_min**2) & np.asarray(x**2+y**2<=radius_max**2)
     x, y, z  = x[selected], y[selected], z[selected]
     px,py,pz =px[selected],py[selected],pz[selected]
     w = w[selected]
